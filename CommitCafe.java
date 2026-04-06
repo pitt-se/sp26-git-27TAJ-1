4ee7007188cab2071684d603260c97a36e4650be
27TAJ <135091928+27TAJ@users.noreply.github.com>
2026-04-06 13:38:55 -0400

    init commit

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,12 +1,13 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Tommy"; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
-        // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println(barista + " just brewed a " + drink);
         // STUDENT_TODO_2B: Add a second brew-related improvement
     }
 
